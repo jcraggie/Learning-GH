9c1903043437b257b911184e8ffcdf9bc071da28
jcrAggie <jcraggie@hotmail.com>
2022-03-07 20:54:09 -0600

    Initial Commit

---
+++ M3_L1_LearningGH/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  M3_L1_LearningGH
+//
+//  Created by Jason Rogers on 3/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ M3_L1_LearningGH/M3_L1_LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  M3_L1_LearningGHApp.swift
+//  M3_L1_LearningGH
+//
+//  Created by Jason Rogers on 3/7/22.
+//
+
+import SwiftUI
+
+@main
+struct M3_L1_LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
